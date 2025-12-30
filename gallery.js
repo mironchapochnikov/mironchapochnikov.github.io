e660fdf277e2510d956dffa6088983b79ac7ac00
mironchapochnikov <m.chapochnikov@gmail.com>
2025-12-30 17:10:14 +0100

    update

--- gallery.js
+++ gallery.js
@@ -403,7 +403,7 @@ function loadCategory(categoryName, clickedElement = null, updateUrl = true) {
     thumbStrip.innerHTML = '';
     currentCategoryList.forEach((filename, index) => {
         const thumb = document.createElement('img');
-        thumb.src = `Images/${filename}`; 
+        thumb.src = `images/${filename}`; 
         thumb.classList.add('thumb-item');
         thumb.addEventListener('click', () => transitionToImage(index));
         thumbStrip.appendChild(thumb);
@@ -431,7 +431,7 @@ function transitionToImage(index) {
 // --- CORE IMAGE UPDATE ---
 function updateMainImageSource(index) {
     currentIndex = index;
-    mainImage.src = `Images/${currentCategoryList[index]}`;
+    mainImage.src = `images/${currentCategoryList[index]}`;
     
     // Counter Logic
     const imageCounter = document.getElementById('image-counter');
